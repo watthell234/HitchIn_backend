599f7b74a7df5ae8417d6436926363379ddd803e
watthell234 <gonzalezjulius.c@gmail.com>
2020-08-06 21:10:23 -0400

    order User query results descending

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def sign_up():
         return jsonify({
             'status': '200',
             'message': 'Successfully Signed Up',
-            'id': db.session.query(User).order_by(desc(created_timestamp)).first()
+            'id': db.session.query(User).order_by(User.created_timestamp.desc()).first()
         })
     else:
         abort(401)
