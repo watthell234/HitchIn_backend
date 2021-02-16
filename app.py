8a4efe11cf38b1f0a307c83218b6500cddac219e
watthell234 <gonzalezjulius.c@gmail.com>
2021-02-16 13:33:11 -0500

    fix to connect db to the prod heroku db

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ from flask_heroku import Heroku
 app = Flask(__name__)
 socketio = SocketIO(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/hitchin'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/hitchin'
 app.config['SECRET_KEY'] = 'super-secret'
 # heroku = Heroku(app)
 db = SQLAlchemy(app)
