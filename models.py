1fb74defd4c5ad7a9c1b1a4ebcf578d29557f820
Joseph Lee <leethfc11@gmail.com>
2021-05-29 18:35:20 -0400

    app.py now checks for existing entries

--- models.py
+++ models.py
@@ -15,7 +15,7 @@ class User(db.Model, TimestampMixin):
     phone_number = db.Column(PhoneNumberType(), unique=True, nullable=False)
     first_name = db.Column(db.String(18), nullable=False)
     last_name = db.Column(db.String(18), nullable=False)
-    email = db.Column(db.String(120), unique=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     is_driver = db.Column(db.Boolean)
 
