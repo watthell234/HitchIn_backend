7168773131f610b376d574bbab1c0b362614a994
Joseph Lee <leethfc11@gmail.com>
2021-06-11 16:00:06 -0400

    test2.

--- app.py
+++ app.py
@@ -272,13 +272,6 @@ def handle_connect():
     json = {
     'sid': request.sid
     }
-    emit('room_ID_connect', json)
-
-@socketio.on('get_room_id')
-def handle_get_room_id():
-    json = {
-    'sid': request.sid
-    }
     emit('room_ID', json)
 
 @socketio.on('disconnect')
