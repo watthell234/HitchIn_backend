fdccef7800635c7a80325bc95a35f86fb60a4b8c
Joseph Lee <leethfc11@gmail.com>
2021-06-24 11:16:40 -0400

    found a bug.

--- app.py
+++ app.py
@@ -275,7 +275,7 @@ def handle_register_trip(data):
     print(pickup)
 
     emit('trip_id_' + carID, {'trip_id': trip.id})
-    emit('updated_car_list' + pickup.replace(" ", "_"), {'car_list': car_list}, broadcast=True)
+    emit('updated_car_list_' + pickup.replace(" ", "_"), {'car_list': car_list}, broadcast=True)
 
 @socketio.on('delete_trip')
 def handle_delete_trip(data):
