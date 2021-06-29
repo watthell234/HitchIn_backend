891e6c24be671c4005b636a33373fce042065e89
Joseph Lee <leethfc11@gmail.com>
2021-06-29 11:44:05 -0400

    start trip test.

--- app.py
+++ app.py
@@ -315,6 +315,7 @@ def handle_start_trip(data):
     print(tripID)
     trip = db.session.query(Trips).filter(Trips.session_id == request.sid).scalar()
 
+    print(trip.id)
 
 #RIDER RELATED
 @socketio.on('init_ride')
