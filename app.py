4fced735ab49f7b1eae4d1c110f2d18b5a81d45e
Joseph Lee <leethfc11@gmail.com>
2021-07-12 15:46:55 -0400

    adding trip history and passenger history if the trip was active.

--- app.py
+++ app.py
@@ -313,6 +313,15 @@ def handle_delete_trip(data):
     #DOESN'T ALWAYS WORK? PROBABLY BECAUSE the client disconnects first before this line gets executed. Not sure.
     trip = db.session.query(Trips).filter(Trips.id == tripID).scalar()
 
+    #Add trip history and passenger history if the trip was acive
+    if trip.active.is_(True):
+        trip_history = TripHistory(trip.driver_id, trip.car_id, trip.pickup, trip.destination, trip.time_started)
+        passenger_rows = db.session.query(Passengers).filter(Passengers.trip_id == trip.id).all()
+        for passenger_row in passenger_rows:
+            passenger_history = PassengerHistory(passenger_row.user_id, passenger_row.trip_id)
+            db.session.add(passenger_history)
+        db.session.add(trip_history)
+
     db.session.delete(trip)
     db.session.commit()
 
