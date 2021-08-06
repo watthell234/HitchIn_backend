a31782448b15c316a44d29cb62907f823ca3881d
Julius Gonzalez <watthell234@users.noreply.github.com>
2021-08-05 21:39:15 -0400

    move mail server info to config class

--- app.py
+++ app.py
@@ -16,19 +16,7 @@ from config import Config, ProdConfig, DevConfig
 
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'super-secret'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# app.config['MAIL_SERVER']='smtp.gmail.com'
-# app.config['MAIL_PORT'] = 465
-# app.config['MAIL_DEFAULT_SENDER'] = 'noreply@hitchinus.com'
-# app.config['MAIL_USERNAME'] = 'noreply@hitchinus.com'
-# app.config['MAIL_PASSWORD'] = 'Keyboard234'
-# app.config['MAIL_USE_TLS'] = False
-# app.config['MAIL_USE_SSL'] = True
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://127.0.0.1/hitchin'
-
-# heroku = Heroku(app)
 myHostName = socket.gethostname()
 if 'local' in myHostName:
     app.config.from_object(DevConfig())
